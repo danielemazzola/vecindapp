787d7dc133ae0b7062547f8a7ca0fb0ccf0f4d55
Daniele Mazzola <mazzoladaniele@gmail.com>
2025-10-29 10:10:36 +0100

    Update model -> license.model & create license_assignmet model

--- src/models/license.model.js
+++ src/models/license.model.js
@@ -4,13 +4,13 @@ const { Schema } = mongoose
 const licenseSchema = new Schema({
   productName: { type: String, required: true, trim: true },
   productKey: { type: String, required: true, unique: true, trim: true },
-  details: {
-    description: { type: String, required: true, trim: true },
-    benefits: { type: [String], default: [], required: true }
-  },
+  description: { type: String, required: true, trim: true },
   userType: { type: String, enum: ['cam', 'esp'], required: true },
   price: { type: Number, required: true, min: 0 },
-  maxBeneficiaries: { type: Number },
+  limits: {
+    maxCommunities: { type: Number, default: 0 }, // CAM
+    maxAdmins: { type: Number, default: 0 },      // ESP
+  },
   isActive: { type: Boolean, default: true },
   durationDays: { type: Number, required: true, default: 30 },
   creatorAdmin: { type: Schema.Types.ObjectId, ref: 'admins', required: true },
---
+++ src/models/licenseAssignment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const licenseAssignmentSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  license: { type: Schema.Types.ObjectId, ref: 'licenses', required: true },
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date },
+  isActive: { type: Boolean, default: true },
+  remainingBeneficiaries: { type: Number, default: 0 },
+}, {
+  collection: 'license_assignments',
+  timestamps: true,
+});
+
+const LICENSE_ASSIGMENT = mongoose.model('license_assignments', licenseAssignmentSchema)
+module.exports = LICENSE_ASSIGMENT
